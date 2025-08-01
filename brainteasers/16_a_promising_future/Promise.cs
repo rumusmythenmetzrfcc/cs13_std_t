833d822e4fa5361d195165a6357ac65ae23875ea
Rumus Mythenmetz <rumus.mythenmetz.afrcc@gmail.com>
2025-08-01 09:06:33 +0200

    Adds brainteaser 16

---
+++ brainteasers/16_a_promising_future/Promise.cs
@@ -0,0 +1,22 @@
+﻿#nullable enable
+
+using System.Diagnostics;
+
+async Task<string[]> SplitWordsAsync(string phrase)
+{
+    // Use of null defaults to splitting on any whitespace character
+    char[]? delimiters = null;
+
+    var words = await Task.Factory.StartNew(() =>
+        phrase.Split(delimiters, StringSplitOptions.RemoveEmptyEntries));
+    
+    return words;
+}
+
+int CountWords()
+{
+    var task = SplitWordsAsync("One Two\nThree");
+    return task.Result.Length;
+}
+
+Print(CountWords());
